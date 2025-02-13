ddc420ed6d6264276c832a0665409fde32ab087b
Sunscar-Sony <2201641530154@psit.ac.in>
2025-02-13 13:49:54 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [playVideo, setPlayVideo] = useState(false);
+
+  const handleLaunch = () => {
+    setPlayVideo(true);
+  };
+
+  const handleVideoEnd = () => {
+    window.location.href = "https://ignitia.in/";
+  };
+
+  return (
+    <div className={playVideo ? "video-container" : "container"}>
+      {!playVideo ? (
+        <div className="center-content">
+          <h1 id="igni">Welcome to Our Site</h1>
+          <button id="igniBtn" onClick={handleLaunch} className="launch-btn">
+            <span style={{color: "black"}}>Launch</span>
+          </button>
+        </div>
+      ) : (
+        <video className="full-video" autoPlay onEnded={handleVideoEnd}>
+          <source src="/preloader.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      )}
+    </div>
+  );
+}
+
+export default App;
